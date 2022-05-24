95ae1322d2f7b1e794bcdf13cfa78606d223f552
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-23 22:53:33 -0500

    Sum Function
    The sum function is implemented and tested.

--- src/main/java/co/com/sofka/calculator/Calculator.java
+++ src/main/java/co/com/sofka/calculator/Calculator.java
@@ -2,4 +2,14 @@ package co.com.sofka.calculator;
 
 public class Calculator {
 
+    static IOperation sum = (a,b) -> {
+        Integer c = a+b;
+        return c;
+    };
+
+    public static void main(String[] args) {
+
+        System.out.println("Sum: " + sum.calculate(5,4));
+
+    }
 }
