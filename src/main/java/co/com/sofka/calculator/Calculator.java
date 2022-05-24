863aab618eaf41c73a4f7023e5caea9e05e14015
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-23 23:01:48 -0500

    Product Function
    The product function from the succession of sums is implemented and tested.

--- src/main/java/co/com/sofka/calculator/Calculator.java
+++ src/main/java/co/com/sofka/calculator/Calculator.java
@@ -12,11 +12,16 @@ public class Calculator {
         return c;
     };
 
+    static Integer product( Integer n, Integer m ) {
+        return (m != 0) ? sum.calculate(n, product(n, m-1)) : 0; // recursive call
+    }
+
 
     public static void main(String[] args) {
 
         System.out.println("Sum: " + sum.calculate(5,4));
         System.out.println("Subtract: " + subtract.calculate(10,5));
+        System.out.println("Product: " + product(6,6));
 
     }
 }
